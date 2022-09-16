f53537edfd44b4465ab88e875ee7cc460af3d2ab
Maliheh <malih.ghasemian@gmail.com>
2022-09-16 11:15:37 +0200

    feat : Create CardController component.

---
+++ src/core/components/CardsController/CardsController.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import * as Styled from './styles';
+
+const CardsController = () => {
+  return <Styled.Container>CardsController</Styled.Container>;
+};
+
+export default CardsController;
