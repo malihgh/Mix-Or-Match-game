9723635bff0a8232bff022c52ceb8b03743884cd
Maliheh <malih.ghasemian@gmail.com>
2022-09-19 22:43:15 +0200

    feat : Expand cards image in cards data.

--- src/core/components/CardsController/data/cards.js
+++ src/core/components/CardsController/data/cards.js
@@ -4,6 +4,8 @@ import Pumpkin from 'assets/Images/Pumpkin.png';
 import Skull from 'assets/Images/Skull.png';
 import Dracula from 'assets/Images/Dracula.png';
 import Bones from 'assets/Images/Bones.png';
+import Bat from 'assets/Images/Bat.png';
+import Cauldron from 'assets/Images/Cauldron.png';
 
 export const cards = [
   {
@@ -44,38 +46,63 @@ export const cards = [
   },
   {
     id: 6,
+    value: 'Bat',
+    image: Bat,
+    isBack: true
+  },
+  {
+    id: 7,
+    value: 'Cauldron',
+    image: Cauldron,
+    isBack: true
+  },
+
+  {
+    id: 8,
     value: 'Eye',
     image: Eye,
     isBack: true
   },
   {
-    id: 7,
+    id: 9,
     value: 'Ghost',
     image: Ghost,
     isBack: true
   },
   {
-    id: 8,
+    id: 10,
     value: 'Pumpkin',
     image: Pumpkin,
     isBack: true
   },
   {
-    id: 9,
+    id: 11,
     value: 'Skull',
     image: Skull,
     isBack: true
   },
   {
-    id: 10,
+    id: 12,
     value: 'Dracula',
     image: Dracula,
     isBack: true
   },
   {
-    id: 11,
+    id: 13,
     value: 'Bones',
     image: Bones,
+    isBack: true
+  },
+  {
+    id: 14,
+    value: 'Bat',
+    image: Bat,
+    isBack: true
+  },
+  {
+    id: 15,
+    value: 'Cauldron',
+    image: Cauldron,
     isBack: true
   }
 ];
