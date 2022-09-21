95b1561937d41099275c48cba8475737adc89cfe
Maliheh <malih.ghasemian@gmail.com>
2022-09-21 16:42:26 +0200

    📱 feat : Change Style for responsive design.

--- src/app/styles.js
+++ src/app/styles.js
@@ -4,5 +4,7 @@ export const Container = styled.div`
   font-family: 'Creepy';
   font-style: normal;
   font-weight: 400;
-  /* background: radial-gradient(#9d5900, #3d2200); */
+  margin: -10px;
+  display: flex;
+  flex-grow: 1;
 `;
